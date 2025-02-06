7dacc1630f12b9ac0045631bb9717de5852ac696
DrorGaon <drordtuii@gmail.com>
2025-02-06 16:42:16 +0200

    Remove unused code

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -68,7 +68,6 @@ async function update(user) {
 			_id: ObjectId.createFromHexString(user._id),
 			username: user.username,
 			fullname: user.fullname,
-			score: user.score,
 		}
 		const collection = await dbService.getCollection('user')
 		await collection.updateOne({ _id: userToSave._id }, { $set: userToSave })
@@ -90,7 +89,6 @@ async function add(user) {
 			username: user.username,
 			password: user.password,
 			fullname: user.fullname,
-			score: user.score || 0,
 		}
 		const collection = await dbService.getCollection('user')
 		await collection.insertOne(userToAdd)
