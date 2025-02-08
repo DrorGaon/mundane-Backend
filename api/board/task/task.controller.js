a35f4122285aa820fd69b9fa51c6fbbdd4cfcc4d
alonWohl <alonw1010@gmail.com>
2025-02-08 13:23:18 +0200

    Add activity tracking to task operations

--- api/board/task/task.controller.js
+++ api/board/task/task.controller.js
@@ -1,109 +1,108 @@
-import { logger } from "../../../services/logger.service.js"
-import { socketService } from "../../../services/socket.service.js"
-import { taskService } from "./task.service.js"
-
+import { logger } from '../../../services/logger.service.js'
+import { socketService } from '../../../services/socket.service.js'
+import { taskService } from './task.service.js'
 
 export async function getTasks(req, res) {
-    try {
-        const { id: boardId, groupId } = req.params
-        const tasks = await taskService.getTasks(boardId, groupId)
-        res.json(tasks)        
-    } catch (err) {
-        logger.error('Failed to get tasks', err)
-        res.status(500).send({ err: 'Failed to get tasks' })
-    }
+  try {
+    const { id: boardId, groupId } = req.params
+    const tasks = await taskService.getTasks(boardId, groupId)
+    res.json(tasks)
+  } catch (err) {
+    logger.error('Failed to get tasks', err)
+    res.status(500).send({ err: 'Failed to get tasks' })
+  }
 }
 
 export async function getTaskById(req, res) {
-    try {
-        const { id: boardId, groupId, taskId } = req.params
-        const task = await taskService.getTaskById(boardId, groupId, taskId)
-        res.json(task)        
-    } catch (err) {
-        logger.error('Failed to get tasks', err)
-        res.status(500).send({ err: 'Failed to get tasks' })
-    }
+  try {
+    const { id: boardId, groupId, taskId } = req.params
+    const task = await taskService.getTaskById(boardId, groupId, taskId)
+    res.json(task)
+  } catch (err) {
+    logger.error('Failed to get tasks', err)
+    res.status(500).send({ err: 'Failed to get tasks' })
+  }
 }
 
 export async function removeTask(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId, groupId, taskId } = req.params
-        const board = await taskService.removeTask(boardId, groupId, taskId)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to remove task', err)
-        res.status(500).send({ err: 'Failed to remove task' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId, groupId, taskId } = req.params
+    const board = await taskService.removeTask(boardId, groupId, taskId)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to remove task', err)
+    res.status(500).send({ err: 'Failed to remove task' })
+  }
 }
 
 export async function addTask(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId, groupId } = req.params
-        const task = req.body
-        const board = await taskService.addTask(boardId, groupId, task)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to add task', err)
-        res.status(500).send({ err: 'Failed to add task' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId, groupId } = req.params
+    const { task, activity } = req.body
+    const board = await taskService.addTask(boardId, groupId, task, activity)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to add task', err)
+    res.status(500).send({ err: 'Failed to add task' })
+  }
 }
 
 export async function updateTask(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId, groupId } = req.params
-        const task = req.body
-        const board = await taskService.updateTask(boardId, groupId, task)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to update task', err)
-        res.status(500).send({ err: 'Failed to update task' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId, groupId } = req.params
+    const { task, activity } = req.body
+    const board = await taskService.updateTask(boardId, groupId, task, activity)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to update task', err)
+    res.status(500).send({ err: 'Failed to update task' })
+  }
 }
 
 export async function removeTasks(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId } = req.params
-        const taskAndGroupIds = req.body
-        const board = await taskService.removeTasks(boardId, taskAndGroupIds)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to remove tasks', err)
-        res.status(500).send({ err: 'Failed to remove tasks' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId } = req.params
+    const taskAndGroupIds = req.body
+    const board = await taskService.removeTasks(boardId, taskAndGroupIds)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to remove tasks', err)
+    res.status(500).send({ err: 'Failed to remove tasks' })
+  }
 }
 
 export async function duplicateTasks(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId } = req.params
-        const tasks = req.body
-        const board = await taskService.duplicateTasks(boardId, tasks)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to duplicate', err)
-        res.status(500).send({ err: 'Failed to duplicate' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId } = req.params
+    const tasks = req.body
+    const board = await taskService.duplicateTasks(boardId, tasks)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to duplicate', err)
+    res.status(500).send({ err: 'Failed to duplicate' })
+  }
 }
 
 export async function archiveTasks(req, res) {
-    try {
-        const { loggedinUser } = req
-        const { id: boardId } = req.params
-        const tasks = req.body
-        const board = await taskService.archiveTasks(boardId, tasks)
-        socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
-        res.json(board)        
-    } catch (err) {
-        logger.error('Failed to remove tasks', err)
-        res.status(500).send({ err: 'Failed to remove tasks' })
-    }
+  try {
+    const { loggedinUser } = req
+    const { id: boardId } = req.params
+    const tasks = req.body
+    const board = await taskService.archiveTasks(boardId, tasks)
+    socketService.broadcast({ type: 'board-updated', data: board, room: boardId, userId: loggedinUser._id })
+    res.json(board)
+  } catch (err) {
+    logger.error('Failed to remove tasks', err)
+    res.status(500).send({ err: 'Failed to remove tasks' })
+  }
 }
--- api/board/task/task.service.js
+++ api/board/task/task.service.js
@@ -1,215 +1,214 @@
-import { ObjectId } from "mongodb"
+import { ObjectId } from 'mongodb'
 
-import { logger } from "../../../services/logger.service.js"
-import { utilService } from "../../../services/util.service.js"
-import { dbService } from "../../../services/db.service.js"
+import { logger } from '../../../services/logger.service.js'
+import { utilService } from '../../../services/util.service.js'
+import { dbService } from '../../../services/db.service.js'
 
 export const taskService = {
-    getTasks,
-    getTaskById,
-    removeTask,
-    addTask,
-    updateTask,
-	removeTasks,
-	duplicateTasks,
-	archiveTasks,
+  getTasks,
+  getTaskById,
+  removeTask,
+  addTask,
+  updateTask,
+  removeTasks,
+  duplicateTasks,
+  archiveTasks
 }
 
-
 async function getTasks(boardId, groupId) {
-	try {
-		const criteria = {
-			_id: ObjectId.createFromHexString(boardId),
-			"groups.id": groupId
-		}
-		  
-		  const projection = {
-			_id: 0,
-			"groups.$": 1
-		}
+  try {
+    const criteria = {
+      _id: ObjectId.createFromHexString(boardId),
+      'groups.id': groupId
+    }
 
-		const collection = await dbService.getCollection('board')
-		const res = await collection.findOne(criteria, {projection})
-		const tasks = res.groups[0].tasks
-		return tasks
-	} catch (err) {
-		logger.error(`while finding tasks in group ${groupId}`, err)
-		throw err
-	}
+    const projection = {
+      _id: 0,
+      'groups.$': 1
+    }
+
+    const collection = await dbService.getCollection('board')
+    const res = await collection.findOne(criteria, { projection })
+    const tasks = res.groups[0].tasks
+    return tasks
+  } catch (err) {
+    logger.error(`while finding tasks in group ${groupId}`, err)
+    throw err
+  }
 }
 
 async function getTaskById(boardId, groupId, taskId) {
-	try {
-		const collection = await dbService.getCollection('board')
+  try {
+    const collection = await dbService.getCollection('board')
 
-		if (groupId === 'null'){
-			const criteria = {
-				_id: ObjectId.createFromHexString(boardId)
-			}
-			  
-			  const projection = {
-				_id: 0,
-				"groups": 1
-			}
-	
-			const res = await collection.findOne(criteria, {projection})
-			const group = res.groups.find(group => {
-				return group.tasks.find((task) => task.id === taskId)
-			  })
-			groupId = group.id
-		}
+    if (groupId === 'null') {
+      const criteria = {
+        _id: ObjectId.createFromHexString(boardId)
+      }
 
+      const projection = {
+        _id: 0,
+        groups: 1
+      }
 
-		const criteria = {
-			_id: ObjectId.createFromHexString(boardId),
-			"groups.id": groupId,
-			"groups.tasks.id": taskId
-		}
-		  
-		  const projection = {
-			_id: 0,
-			"groups.$": 1
-		}
+      const res = await collection.findOne(criteria, { projection })
+      const group = res.groups.find((group) => {
+        return group.tasks.find((task) => task.id === taskId)
+      })
+      groupId = group.id
+    }
 
-		const res = await collection.findOne(criteria, {projection})
+    const criteria = {
+      _id: ObjectId.createFromHexString(boardId),
+      'groups.id': groupId,
+      'groups.tasks.id': taskId
+    }
 
-		const task = res.groups[0].tasks.find(task => task.id === taskId)
-		return task
-	} catch (err) {
-		logger.error(`while finding tasks in group ${groupId}`, err)
-		throw err
-	}
+    const projection = {
+      _id: 0,
+      'groups.$': 1
+    }
+
+    const res = await collection.findOne(criteria, { projection })
+
+    const task = res.groups[0].tasks.find((task) => task.id === taskId)
+    return task
+  } catch (err) {
+    logger.error(`while finding tasks in group ${groupId}`, err)
+    throw err
+  }
 }
 
 async function removeTask(boardId, groupId, taskId, isBulkAction = false) {
-	try {
-		const criteria = {
-			_id: ObjectId.createFromHexString(boardId),
-			"groups.id": groupId,
-		}
-	
-		const remove = { 
-			$pull: { "groups.$.tasks": { id: taskId } } 
-		}
+  try {
+    const criteria = {
+      _id: ObjectId.createFromHexString(boardId),
+      'groups.id': groupId
+    }
 
-		const collection = await dbService.getCollection('board')
-		const { modifiedCount } = await collection.updateOne(criteria, remove)
+    const remove = {
+      $pull: { 'groups.$.tasks': { id: taskId } }
+    }
 
-		if (!modifiedCount) throw new Error('problem removing task')
-		if (!isBulkAction) return await collection.findOne({_id: ObjectId.createFromHexString(boardId)})
-	} catch (err) {
-		logger.error(`while finding tasks in group ${groupId}`, err)
-		throw err
-	}
+    const collection = await dbService.getCollection('board')
+    const { modifiedCount } = await collection.updateOne(criteria, remove)
+
+    if (!modifiedCount) throw new Error('problem removing task')
+    if (!isBulkAction) return await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+  } catch (err) {
+    logger.error(`while finding tasks in group ${groupId}`, err)
+    throw err
+  }
 }
 
-async function addTask(boardId, groupId, task, isDuplicate = false) {
-	try {
-		task.id = utilService.makeId()
+async function addTask(boardId, groupId, task, activity, isDuplicate = false) {
+  console.log('🚀 ~ addTask ~ activity:', activity)
 
-		const criteria = {
-			_id: ObjectId.createFromHexString(boardId),
-			"groups.id": groupId,
-		}
-	
-		const add = isDuplicate 
-		? {
-        	$push: {
-        	  "groups.$.tasks": {
-        	    $each: [task],
-        	    $position: task.idx + 1
-        	  	}
-        	}
-		}
-		: { 
-			$push: { "groups.$.tasks": task } 
-		}
-		
-		if(isDuplicate) {
-			delete task.idx
-			delete task.groupId
-			task.title += ' (copy)'
-		}
+  try {
+    task.id = utilService.makeId()
 
-		const collection = await dbService.getCollection('board')
-		const { modifiedCount } = await collection.updateOne(criteria, add)
+    const criteria = {
+      _id: ObjectId.createFromHexString(boardId),
+      'groups.id': groupId
+    }
 
-		if (!modifiedCount) throw new Error('problem adding task')
-		if (!isDuplicate) return await collection.findOne({_id: ObjectId.createFromHexString(boardId)})
-	} catch (err) {
-		logger.error(`while finding tasks in group ${groupId}`, err)
-		throw err
-	}
+    const add = isDuplicate
+      ? {
+          $push: {
+            'groups.$.tasks': {
+              $each: [task],
+              $position: task.idx + 1
+            }
+          }
+        }
+      : {
+          $push: { 'groups.$.tasks': task }
+        }
+
+    if (isDuplicate) {
+      delete task.idx
+      delete task.groupId
+      task.title += ' (copy)'
+    }
+
+    const collection = await dbService.getCollection('board')
+    const { modifiedCount } = await collection.updateOne(criteria, add)
+
+    if (!modifiedCount) throw new Error('problem adding task')
+    if (!isDuplicate) return await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+  } catch (err) {
+    logger.error(`while finding tasks in group ${groupId}`, err)
+    throw err
+  }
 }
 
-async function updateTask(boardId, groupId, task) {
-	try {
-		const criteria = {
-			_id: ObjectId.createFromHexString(boardId),
-			"groups.id": groupId,
-		}
-	
-		const update = {
-			$set: {
-			  "groups.$.tasks.$[elem]": task  
-			}
-		}
+async function updateTask(boardId, groupId, task, activity) {
+  console.log('🚀 ~ updateTask ~ activity:', activity)
+  try {
+    const criteria = {
+      _id: ObjectId.createFromHexString(boardId),
+      'groups.id': groupId
+    }
 
-		const arrayFilters = [
-			{ "elem.id": task.id }  
-		]
+    const update = {
+      $set: {
+        'groups.$.tasks.$[elem]': task
+      }
+    }
 
-		const collection = await dbService.getCollection('board')
-		const { modifiedCount } = await collection.updateOne(criteria, update, { arrayFilters })
-        
-		if (!modifiedCount) throw new Error('problem updating task')
-		return await collection.findOne({_id: ObjectId.createFromHexString(boardId)})
-	} catch (err) {
-		logger.error(`while finding tasks in group ${groupId}`, err)
-		throw err
-	}
+    const arrayFilters = [{ 'elem.id': task.id }]
+
+    const collection = await dbService.getCollection('board')
+    const { modifiedCount } = await collection.updateOne(criteria, update, { arrayFilters })
+
+    if (!modifiedCount) throw new Error('problem updating task')
+    return await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+  } catch (err) {
+    logger.error(`while finding tasks in group ${groupId}`, err)
+    throw err
+  }
 }
 
-async function removeTasks( boardId, taskAndGroupIds ) {
-	try {
-		for (const ids of taskAndGroupIds) {
-			await removeTask(boardId, ids.groupId, ids.taskId)
-		}
-		const collection = await dbService.getCollection('board')
-		return await collection.findOne({_id: ObjectId.createFromHexString(boardId)})	
-	} catch (err) {
-		logger.error(`while removing tasks`, err)
-		throw err
-	}
-}	
+async function removeTasks(boardId, taskAndGroupIds) {
+  try {
+    for (const ids of taskAndGroupIds) {
+      await removeTask(boardId, ids.groupId, ids.taskId)
+    }
+    const collection = await dbService.getCollection('board')
+    return await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+  } catch (err) {
+    logger.error(`while removing tasks`, err)
+    throw err
+  }
+}
 
-async function duplicateTasks( boardId, tasks ) {
-	try {
-		const collection = await dbService.getCollection('board')
-		const board = await collection.findOne({_id: ObjectId.createFromHexString(boardId)})
-		for (const task of [...tasks].reverse()) {
-			const group = board.groups.find(group => group.id === task.groupId)
-			task.idx = group.tasks.findIndex(t => t.id === task.id)
-			await addTask(boardId, group.id, task, true)
-		}
-		return await collection.findOne({_id: ObjectId.createFromHexString(boardId)})	
-	} catch (err) {
-		logger.error(`while duplicating tasks`, err)
-		throw err
-	}
-}	
+async function duplicateTasks(boardId, tasks) {
+  try {
+    const collection = await dbService.getCollection('board')
+    const board = await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+    for (const task of [...tasks].reverse()) {
+      const group = board.groups.find((group) => group.id === task.groupId)
+      task.idx = group.tasks.findIndex((t) => t.id === task.id)
+      await addTask(boardId, group.id, task, true)
+    }
+    return await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+  } catch (err) {
+    logger.error(`while duplicating tasks`, err)
+    throw err
+  }
+}
 
-async function archiveTasks(boardId, tasks){
-	try {
-		const updatedTasks = tasks.map(task => ({...task, archivedAt: Date.now()}))
-		for (const task of updatedTasks) {
-			await updateTask(boardId, task.groupId, task)
-		}
-		const collection = await dbService.getCollection('board')
-		const board = await collection.findOne({_id: ObjectId.createFromHexString(boardId)})
-		return board
-	} catch (err) {
-		logger.error(`while archiving tasks`, err)
-		throw err
-	}
+async function archiveTasks(boardId, tasks) {
+  try {
+    const updatedTasks = tasks.map((task) => ({ ...task, archivedAt: Date.now() }))
+    for (const task of updatedTasks) {
+      await updateTask(boardId, task.groupId, task)
+    }
+    const collection = await dbService.getCollection('board')
+    const board = await collection.findOne({ _id: ObjectId.createFromHexString(boardId) })
+    return board
+  } catch (err) {
+    logger.error(`while archiving tasks`, err)
+    throw err
+  }
 }
